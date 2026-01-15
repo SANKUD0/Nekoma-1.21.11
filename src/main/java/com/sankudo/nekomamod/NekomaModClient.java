d0bf7e291a7975e3a1628955489234e5470f8220
SANKUD0 <tacofauche@gmail.com>
2026-01-15 14:31:21 -0500

    Initial commit

---
+++ src/main/java/com/sankudo/nekomamod/NekomaModClient.java
@@ -0,0 +1,10 @@
+package com.sankudo.nekomamod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NekomaModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
